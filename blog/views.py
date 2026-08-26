06d76c1ec8d1c2cfab819422a5dbf385ebaa2658
Victor <novovitya@yandex.ru>
2026-08-26 22:26:46 +0300

    Class views

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,49 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from django.http import Http404
 from .models import Post
 
 
+
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 
-                  'blog/post/list.html', 
+    all_posts  = Post.published.all()
+    paginator = Paginator(all_posts, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+            # Если page_number не целое число, то
+            # выдать первую страницу
+            posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
                   {'posts': posts})
 
 
-def post_detail(request, id):
+
+def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post,
-                             id=id, status=Post.Status.PUBLISHED)
-    
+                             status=Post.Status.PUBLISHED,
+                             slug=slug,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
