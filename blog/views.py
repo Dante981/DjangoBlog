3bbeed1d9705cbedd93abb4aa8bc72a1575a3653
Victor <novovitya@yandex.ru>
2026-08-27 21:18:37 +0300

    add send mail

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.http import Http404
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Post
 from .forms import EmailPostForm
 
@@ -22,17 +24,30 @@ def post_share(request, post_id):
     post = get_object_or_404(Post,
                              id=post_id,
                              status=Post.Status.PUBLISHED)
+    sent = False
+
     if request.method == 'POST':
         # Форма была передана на обработку
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Поля формы успешно прошли валидацию
             cd = form.cleaned_data
-            # ... отправить электронное письмо
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s ({cd['email']}) comments: {cd['comments']}"
+            print(settings.MAILERS)
+            send_mail(subject, message, settings.MAILERS['default']['OPTIONS']['username'],
+                      [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form})
+                                                    'form': form,
+                                                    'sent': sent})
 # Create your views here.
 def post_list(request):
     all_posts  = Post.published.all()
