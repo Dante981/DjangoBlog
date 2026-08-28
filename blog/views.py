50e1faebfaba3b9682942500cc916d94dbbeb8c5
Victor <novovitya@yandex.ru>
2026-08-28 22:06:21 +0300

    add coments

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
-from django.http import Http404
 from django.core.mail import send_mail
 from django.conf import settings
+
+from django.views.generic import ListView
+from django.views.decorators.http import require_POST
+from django.http import Http404
+
+
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 
@@ -39,7 +44,7 @@ def post_share(request, post_id):
                       f"{post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
                       f"{cd['name']}\'s ({cd['email']}) comments: {cd['comments']}"
-            print(settings.MAILERS)
+
             send_mail(subject, message, settings.MAILERS['default']['OPTIONS']['username'],
                       [cd['to']])
             sent = True
@@ -76,6 +81,29 @@ def post_detail(request, year, month, day, slug):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, 
+                             id=post_id,  
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', 
+                 {'post': post, 
+                  'form': form, 
+                  'comment': comment})
+
